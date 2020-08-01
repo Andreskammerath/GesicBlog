fd9445088b2e4beab62d73546b97e1b22168785d
Andres Kammerath <andreskammerath@gmail.com>
2020-07-31 22:56:23 -0300

    Some css added to the list Blog site

--- GesicBlog/blog/views.py
+++ GesicBlog/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 def index(request):
-	posts = Post.objects.all().order_by('created_date')
+	posts = Post.objects.all().order_by('-created_date')
 	return render(request, "blog/listBlog.html", {
 		'posts' : posts
 		})
