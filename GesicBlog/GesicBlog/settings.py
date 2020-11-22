ff41c8e911f825052bebc908f13d958988d3afb9
Andres Kammerath <43712227+Andreskammerath@users.noreply.github.com>
2020-11-22 17:57:22 -0300

    Update settings.py

--- GesicBlog/GesicBlog/settings.py
+++ GesicBlog/GesicBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'gesic-blog.herokuapp.com','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', 'gesic-blog.herokuapp.com','0.0.0.0','gesicmediacion.com']
 
 
 # Application definition
